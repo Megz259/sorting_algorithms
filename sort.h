684c15722ebc3ccfddb39c4c59ec79ea6e5d7906
Megz259 <125645532+Megz259@users.noreply.github.com>
2024-04-30 09:03:48 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,6 +2,22 @@
 #define SORT_H
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list);
 
 /**
  * struct listint_s - Doubly linked list node
